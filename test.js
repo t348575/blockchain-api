9d66286f0a8a26c224eceb6ec148925cb436a9aa
Joseph Subash Kanichai <40432493+t348575@users.noreply.github.com>
2020-03-19 19:52:53 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,16 @@
-let { BlockChainAPI } = require('./BlockChainAPI');
-blockchainAPI = new BlockChainAPI({'dsa':'dsa'});
-blockchainAPI.addBlock({ difficulty: 3, prev_hash: 'de04d58dc5ccc4b9671c3627fb8d626fe4a15810bc1fe3e724feea761965fb71', data: 'first block' }, new Date().getTime()).then(res => {
-    console.log('Result: ', JSON.parse(res));
-    next();
-}).catch(err => {
-    console.log('Error: ', err);
+const blockchainAPI = new testAddon.BlockChainWrapper();
+sha2.genesis({ difficulty: 3, prev_hash: 'de04d58dc5ccc4b9671c3627fb8d626fe4a15810bc1fe3e724feea761965fb71', data: 'first block' }).then(result => {
+    console.log(JSON.parse(result));
+    runRest();
 });
-function next() {
-    blockchainAPI.addBlock({ data: 'second block'}).then(res => {
-        console.log('Result: ', JSON.parse(res));
-    }).catch(err => {
-        console.log('Error: ', err);
+function runRest() {
+    sha2.addBlock({ data: "second block" }).then(result => {
+        console.log(JSON.parse(result));
     });
-    blockchainAPI.addBlock({ data: 'third block'}).then(res => {
-        console.log('Result: ', JSON.parse(res));
-    }).catch(err => {
-        console.log('Error: ', err);
+    sha2.addBlock({ data: "third block" }).then(result => {
+        console.log(JSON.parse(result));
     });
-    blockchainAPI.addBlock({ data: 'fourth block'}).then(res => {
-        console.log('Result: ', JSON.parse(res));
-    }).catch(err => {
-        console.log('Error: ', err);
+    sha2.addBlock({ data: "fourth block" }).then(result => {
+        console.log(JSON.parse(result));
     });
 }
