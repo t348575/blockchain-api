45b0586d8a53bb61996d85f017f215f0da0f3d1c
Joseph Subash Kanichai <40432493+t348575@users.noreply.github.com>
2020-03-19 19:55:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-const blockchainAPI = new testAddon.BlockChainWrapper();
-sha2.genesis({ difficulty: 3, prev_hash: 'de04d58dc5ccc4b9671c3627fb8d626fe4a15810bc1fe3e724feea761965fb71', data: 'first block' }).then(result => {
+const blockchainAPI = new (require('./build/Release/blockchainNativeApi.node')).BlockChainWrapper();
+blockchainAPI.genesis({ difficulty: 3, prev_hash: 'de04d58dc5ccc4b9671c3627fb8d626fe4a15810bc1fe3e724feea761965fb71', data: 'first block' }).then(result => {
     console.log(JSON.parse(result));
     runRest();
 });
 function runRest() {
-    sha2.addBlock({ data: "second block" }).then(result => {
+    blockchainAPI.addBlock({ data: "second block" }).then(result => {
         console.log(JSON.parse(result));
     });
-    sha2.addBlock({ data: "third block" }).then(result => {
+    blockchainAPI.addBlock({ data: "third block" }).then(result => {
         console.log(JSON.parse(result));
     });
-    sha2.addBlock({ data: "fourth block" }).then(result => {
+    blockchainAPI.addBlock({ data: "fourth block" }).then(result => {
         console.log(JSON.parse(result));
     });
 }
